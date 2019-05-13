27761f26acb5db8fd4ec2523c5b34fadd106f69e
Joseph Rubin <josephmeirrubin@gmail.com>
2019-05-13 02:45:05 -0400

    compute eigenvectors

--- main.py
+++ main.py
@@ -3,29 +3,48 @@ import collect
 import matplotlib.pyplot as plt
 
 def main():
-    #crop_data = open("crop_data", "r")
-
+    # Retrieve the images from the disk.
     images = collect.get_images(1)
 
+    # Crop our images to center the face.
     images_cropped = [img[5:240, 90:290] for img in images]
 
+    # Turn the images into vectors.
     images_vec = [img.flatten() for img in images_cropped]
 
+    # Calculate the difference from the mean for each frame.
     mean = np.zeros(235 * 200).flatten()
     for img in images_vec:
         mean = np.add(mean, img)
 
     mean /= len(images_vec)
-    print(mean)
+    #print(mean)
 
+    difs = []
     for img in images_vec:
         i = img - mean
+        difs.append(i)
 
-    mean.resize(235, 200)
-    print(mean)
-    plt.matshow(mean)
-    plt.show()
+    A = np.column_stack(difs)
 
+    C = np.matmul(A.T, A)
+
+    (values, vectors) = np.linalg.eig(C)
+
+    vectors2 = []
+    for v in vectors:
+        vv = A.dot(v)
+        vvv = vv / np.linalg.norm(vv)
+        vectors2.append(vvv)
+
+    print(vectors2)
+
+    #print(values)
+
+    #mean.resize(235, 200)
+    #print(mean)
+    #plt.matshow(mean)
+    #plt.show()
 
 
 main()
